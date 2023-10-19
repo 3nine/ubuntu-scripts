d7f2824ec30c4fe45fd0646e38a0db58973c4562
hxnning <106799256+3nine@users.noreply.github.com>
2023-10-19 17:43:53 +0200

    Update docker_install.sh

--- install/docker_install.sh
+++ install/docker_install.sh
@@ -1,1 +1,14 @@
+#!/bin/bash
+# Version 1.0
+# Copyright (c) 2023 3nine
+# Author: 3nine
+# License: MIT
+# https://github.com/3nine/pi/main/LICENSE.md
 
+# Setze die Farben für die Ausgabe
+GREEN='\e[32m'
+BLUE='\e[34m'
+CYAN='\e[36m'
+YELLOW='\e[33m'
+RED='\e[31m'
+RESET='\e[0m'
