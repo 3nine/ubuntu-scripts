a14ba0c25d3dba1dcee01737802f0dbe199da377
hxnning <106799256+3nine@users.noreply.github.com>
2023-10-19 17:04:21 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,9 @@
-
+#!/bin/bash
+# Version 1.6
+# Copyright (c) 2023 tteck
+# Author: 3nine
+# License: MIT
+# https://github.com/3nine/pi/init.sh
 
 # Setze die Farben für die Ausgabe
 GREEN='\e[32m'
@@ -30,11 +35,11 @@ check_docker_installed() {
   fi
 }
 
-check_docker-compose_installed() {
+check_docker_compose_installed() {
   if dpkg -l | grep -q "docker-compose"; then
-    docker-compose_installed=true
+    docker_compose_installed=true
   else
-    docker-compose_installed=false
+    docker_compose_installed=false
   fi
 }
 
@@ -44,7 +49,7 @@ install_docker() {
   echo -e "${BLUE}Docker wurde installiertt.${RESET}"
 }
 
-install_docker-compose() {
+install_docker_compose() {
   echo -e "${BLUE}Installiere Docker-Compose...${RESET}"
   sudo apt install docker-compose-plugin
   echo -e "${BLUE}Docker-Compose wurde installiert.${RESET}"
@@ -77,6 +82,95 @@ fi
 sudo echo "Um dieses Skript auszuführen, sind Root Berechtigungen nötig."
 
 echo -e "Grundinstallation eines Pi's"
+# Führt ein Update der Paketquellen durch
+echo "Aktualisiere Paketquellen"
+sudo apt update > /dev/null 2>&1
+echo -e "${GREEN}Aktualisierung abgeschlossen${RESET}"
+
+# Führt ein Upgrade der Paketquellen durch
+echo "Upgrade der installierten Paketquellen"
+sudo apt update > /dev/null 2>&1
+echo -e "${GREEN}Upgrade abgeschlossen${RESET}"
+
+# Aufruf function autoremove
+autoremove
+
+clear
+check_dockerinstalled
+if $docker_installed; then
+  echo -e "${YELLOW}Docker ist bereits installiert, daher wird dieser Schritt übersprungen!${RESET}"
+else
+  # Abfrage Docker Installation
+  dialog --title "Docker Installation" --yesno "Möchten Sie Docker installieren?" 0 0
+  response_docker=$?
+  case $response_docker in
+    0)
+      clear
+      install_docker ;; # Benutzer möchte Docker installieren
+
+      # Teste ob Docker-Compose mit installiert wurde.
+      check_docker_compose_installed
+      if $docker_compose_installed; then
+        clear
+      else
+        # Abfrage Docker-Compose Installation
+         dialog --title "Docker-Compose Installation" --yesno "Möchten Sie Docker-Compose  installieren?" 0 0
+         response_docker_compose=$?
+         case $response_docker_compose
+           0)
+             clear
+             install_docker_compose ;; # Docker Compose wurde nicht installiert aber der Benutzer möchte es installieren
+           1)
+             echo -e "${GREEN}Docker wurde nicht installiert.${RESET}" # Benutzer möchte Docker-Compose nicht installieren
+           255)
+             echo -e "${RED}Abbruch.${RESET}" ;; # Benutzer hat abbruch gewählt
+    1)
+      echo -e "${GREEN}Docker wurde nicht installiert.${RESET}" #Benutzer möchte Docker nicht installieren
+    255)
+      echo -e "${RED}Abbruch.${RESET}" ;; # Benutzer hat abbruch gewählt
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Autoupdate Abfrage
+dialog --title "Autoupdate aktivieren" --yesno "Möchten Sie Autoupdate aktivieren? Wenn ja, werden wöchentliche Updates automatisch Samstags um 00:00 Uhr durchgeführt." 0 0
+
+response=$?
+case $response in
+  0)
+    echo -e "${YELLOW}Autoupdate wird aktiviert.${RESET}"
+    sudo mkdir -p /opt/update/
+    sudo curl -o /opt/update/update-script.sh https://raw.githubusercontent.com/3nine/pi/auto_update.sh
+    sudo chmod +x /opt/update/auto_update.sh
+    (crontab -l ; echo "0 0 * * 6 /opt/update/auto_update.sh") | crontab -
+    echo -e "${BLUE}Autoupdate aktiviert.${RESET}"
+    ;;
+  1)
+    echo -e "${GREEN}Autoupdate wird nicht aktiviert.${RESET}" ;;
+  255)
+    echo -e "${YELLOW}Abbruch.${RESET}" ;;
+esac
+
+
+
 
 
 
