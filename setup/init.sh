5e34c8653b7b7218392254b6f3e5105d7eb8b802
hxnning <106799256+3nine@users.noreply.github.com>
2023-11-20 15:54:52 +0100

    Update init.sh

--- setup/init.sh
+++ setup/init.sh
@@ -89,6 +89,7 @@ CHOICE=$?
     interface="eth0"
 
     sudo touch /etc/netplan/01-netcfg.yaml
+    sudo chmod 700 /etc/netplan/01-netcfg.yaml
     sudo cat > /etc/netplan/01-netcfg.yaml <<EOL
 network:
   version: 2
@@ -101,9 +102,9 @@ network:
           via: $gateway
       nameservers: [$dns1,$dns2]
 EOL
-    sudo chmod 700 /etc/netplan/01-netcfg.yaml
+    sudo chmod 600 /etc/netplan/01-netcfg.yaml
     sudo netplan apply > /dev/null 2>&1
-    sudo rm -r /etc/netplan/00-installer-config.yaml
+    sudo rm -r /etc/netplan/00-installer-config.yaml > /dev/null 2>&1
     msg_ok "Changed IP address"
     ;;
   1)
