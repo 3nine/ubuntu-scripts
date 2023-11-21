5d3c25ab01a875ca168748a221ab390faa7c9efd
hxnning <106799256+3nine@users.noreply.github.com>
2023-11-21 08:14:12 +0100

    Update ubuntu-configuration.sh

--- setup/ubuntu-configuration.sh
+++ setup/ubuntu-configuration.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Version 1.6
+# Version 1.0
 # Copyright (c) 2023 3nine
 # Author: 3nine
 # License: MIT
@@ -16,6 +16,14 @@ cat <<"EOF"
     \____/  |_.__/   \__,_| |_| |_|  \__|  \__,_|
     
 EOF
+}
+
+show_summary() {
+  show_header
+  if [ $sum_network ]; then
+    msg_ok "Network configured"
+  else
+    msg_info " No Network configured"
 }
 
 # Setze die Farben für die Ausgabe
@@ -56,13 +64,13 @@ start_configuration() {
   CHOICE=$?
     case $CHOICE in
     0)
-      ipv4_regex="^([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$"
       sudo apt install netplan -y > /dev/null 2>&1
       ipadress=$(sudo dialog --title "IP-Adresse" --inputbox "Legen Sie eine Ip-Adresse fest. (Format: X.X.X.X)" 0 0 2>&1 >/dev/tty)
       subnet_mask=$(sudo dialog --title "IP-Adresse" --inputbox "Legen Sie eine Subnetzmaske fest. (Format: /XX)" 0 0 2>&1 >/dev/tty)
       gateway=$(sudo dialog --title "IP-Adresse" --inputbox "Legen Sie ein Standard-Gateway fest." 0 0 2>&1 >/dev/tty)
       dns1=$(sudo dialog --title "IP-Adresse" --inputbox "Legen Sie den ersten DNS Server fest." 0 0 2>&1 >/dev/tty)
       dns2=$(sudo dialog --title "IP-Adresse" --inputbox "Legen Sie den zweiten DNS Server fest." 0 0 2>&1 >/dev/tty)
+      
       interface="eth0"
   
       sudo touch /etc/netplan/01-netcfg.yaml
@@ -96,12 +104,14 @@ EOL
       sudo curl -o /opt/update/auto_update.sh https://raw.githubusercontent.com/3nine/pi/main/setup/auto_update.sh
       sudo chmod +x /opt/update/auto_update.sh
       (crontab -l ; echo "0 0 * * 6 /opt/update/auto_update.sh") | crontab -
-      msg_ok "Automatic Updates activated"
+      sum_network=true
       ;;
     1)
-      msg_error "Selected no to auto-update"
+      sum_network=false
       ;;
     esac
+
+    show_summary
 }
 
 show_header
