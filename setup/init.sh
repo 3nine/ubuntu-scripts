f9d62924cdc76ea7024071beda539d16d756743c
hxnning <106799256+3nine@users.noreply.github.com>
2023-11-21 07:08:07 +0100

    Update init.sh

--- setup/init.sh
+++ setup/init.sh
@@ -65,64 +65,55 @@ pause() {
   sleep 2 # 2 Sekunden Pause
 }
 
-# ----------------------------------------------------------------> Start <----------------------------------------------------------------
-
-# Prüfe ob Arg1 "?" oder "help" ist
-if [ "$1" = "?" ] || [ "$1" = "help" ]; then
-  show_help
-fi
-
-show_header
-sudo_cache
-
-dialog --title "Network Configuration" --yesno "Do you want to change your current IP address?" 0 0
-CHOICE=$?
-  case $CHOICE in
-  0)
-    ipv4_regex="^([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$"
-    sudo apt install netplan -y > /dev/null 2>&1
-    ipadress=$(sudo dialog --title "IP-Adresse" --inputbox "Legen Sie eine Ip-Adresse fest. (Format: X.X.X.X)" 0 0 2>&1 >/dev/tty)
-    subnet_mask=$(sudo dialog --title "IP-Adresse" --inputbox "Legen Sie eine Subnetzmaske fest. (Format: /XX)" 0 0 2>&1 >/dev/tty)
-    gateway=$(sudo dialog --title "IP-Adresse" --inputbox "Legen Sie ein Standard-Gateway fest." 0 0 2>&1 >/dev/tty)
-    dns1=$(sudo dialog --title "IP-Adresse" --inputbox "Legen Sie den ersten DNS Server fest." 0 0 2>&1 >/dev/tty)
-    dns2=$(sudo dialog --title "IP-Adresse" --inputbox "Legen Sie den zweiten DNS Server fest." 0 0 2>&1 >/dev/tty)
-    interface="eth0"
-
-    sudo touch /etc/netplan/01-netcfg.yaml
-    sudo chmod 766 /etc/netplan/01-netcfg.yaml
-    sudo cat > /etc/netplan/01-netcfg.yaml <<EOL
-network:
-  version: 2
-  ethernets:
-    $interface:
-      addresses:
-        - $ipadress/$subnet_mask
-      routes:
-        - to: default
-          via: $gateway
-      nameservers: [$dns1,$dns2]
-EOL
-    sudo netplan apply > /dev/null 2>&1
-    sudo rm -r /etc/netplan/00-installer-config.yaml > /dev/null 2>&1
-    msg_ok "Changed IP address"
-    ;;
-  1)
-    msg_error "Selected no to change the IP address"
-    ;;
-  esac
-
-  dialog --title "Automatic Updates" --yesno "Do you want to activate automatic updates?" 0 0
+start_configuration() {
+  dialog --title "Network Configuration" --yesno "Do you want to change your current IP address?" 0 0
   CHOICE=$?
-  case $CHOICE in
-  0)
-    sudo mkdir -p /opt/update/
-    sudo curl -o /opt/update/auto_update.sh https://raw.githubusercontent.com/3nine/pi/main/setup/auto_update.sh
-    sudo chmod +x /opt/update/auto_update.sh
-    (crontab -l ; echo "0 0 * * 6 /opt/update/auto_update.sh") | crontab -
-    msg_ok "Automatic Updates activated"
-    ;;
-  1)
-    msg_error "Selected no to auto-update"
-    ;;
-  esac
-
+    case $CHOICE in
+    0)
+      ipv4_regex="^([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$"
+      sudo apt install netplan -y > /dev/null 2>&1
+      ipadress=$(sudo dialog --title "IP-Adresse" --inputbox "Legen Sie eine Ip-Adresse fest. (Format: X.X.X.X)" 0 0 2>&1 >/dev/tty)
+      subnet_mask=$(sudo dialog --title "IP-Adresse" --inputbox "Legen Sie eine Subnetzmaske fest. (Format: /XX)" 0 0 2>&1 >/dev/tty)
+      gateway=$(sudo dialog --title "IP-Adresse" --inputbox "Legen Sie ein Standard-Gateway fest." 0 0 2>&1 >/dev/tty)
+      dns1=$(sudo dialog --title "IP-Adresse" --inputbox "Legen Sie den ersten DNS Server fest." 0 0 2>&1 >/dev/tty)
+      dns2=$(sudo dialog --title "IP-Adresse" --inputbox "Legen Sie den zweiten DNS Server fest." 0 0 2>&1 >/dev/tty)
+      interface="eth0"
+  
+      sudo touch /etc/netplan/01-netcfg.yaml
+      sudo chmod 766 /etc/netplan/01-netcfg.yaml
+      sudo cat > /etc/netplan/01-netcfg.yaml <<EOL
+  network:
+    version: 2
+    ethernets:
+      $interface:
+        addresses:
+          - $ipadress/$subnet_mask
+        routes:
+          - to: default
+            via: $gateway
+        nameservers: [$dns1,$dns2]
+EOL
+      sudo netplan apply > /dev/null 2>&1
+      sudo rm -r /etc/netplan/00-installer-config.yaml > /dev/null 2>&1
+      msg_ok "Changed IP address"
+      ;;
+    1)
+      msg_error "Selected no to change the IP address"
+      ;;
+    esac
+  
+    dialog --title "Automatic Updates" --yesno "Do you want to activate automatic updates?" 0 0
+    CHOICE=$?
+    case $CHOICE in
+    0)
+      sudo mkdir -p /opt/update/
+      sudo curl -o /opt/update/auto_update.sh https://raw.githubusercontent.com/3nine/pi/main/setup/auto_update.sh
+      sudo chmod +x /opt/update/auto_update.sh
+      (crontab -l ; echo "0 0 * * 6 /opt/update/auto_update.sh") | crontab -
+      msg_ok "Automatic Updates activated"
+      ;;
+    1)
+      msg_error "Selected no to auto-update"
+      ;;
+    esac
+}
